973b2bb98d13b0f29f91c69e1a006e7ead985ac1
5hif7y <44300833+5hif7y@users.noreply.github.com>
2024-10-13 19:48:49 -0300

    test fix

---
+++ your_program.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Use this script to run your program LOCALLY.
+REM Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+
+setlocal
+
+REM Exit early if any commands fail
+set "ERRORLEVEL=0"
+
+REM Ensure compile steps are run within the repository directory
+cd /d "%~dp0" || exit /b 1
+
+REM Edit this to change how your program compiles locally
+mvn -B package -Ddir=/tmp/codecrafters-build-kafka-java
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+REM Edit this to change how your program runs locally
+java -jar /tmp/codecrafters-build-kafka-java/codecrafters-kafka.jar %*
+
