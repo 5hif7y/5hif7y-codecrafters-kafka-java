69c3544c2877f0e5947b541792b5f6164c24c0c9
5hif7y <44300833+5hif7y@users.noreply.github.com>
2024-10-13 23:55:56 -0300

    Task 5 Handle APIVersions requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,18 +29,19 @@ public class Main {
        byte[] request_api_key = in.readNBytes(2); // INT16, 2 bytes
        byte[] request_api_version = in.readNBytes(2); // INT16, 2 bytes
        byte[] correlation_id = in.readNBytes(4); //INT32, 4 bytes
-       byte[] client_id; // Nullable String
-       byte[] tagged_fields; // TAGGED_FIELDS
+       // client_id --- Nullable String --- Fixed to MAX INT16, 2 bytes, not sure if max INT64, 8 bytes
+       short client_id_length = ByteBuffer.wrap(in.readNBytes(2)).getShort();
+       byte[] client_id = client_id_length > 0 ? in.readNBytes(client_id_length) : new byte[0];
+       byte[] tagged_fields = in.readNBytes(1); // TAGGED_FIELDS
 
        // Output
        OutputStream out = clientSocket.getOutputStream();
-       out.write(message_size);
-       out.write(correlation_id);
-
-       short version = ByteBuffer.wrap(request_api_version).getShort();
-       if(version < 0 || version > 4){
-	       out.write(new byte[] {0, 35});
-       }
+       /*
+       */
+       ByteBuffer outputBuffer = ByteBuffer.allocate(128);
+       processOutput(outputBuffer, message_size, correlation_id, request_api_key,
+		       request_api_version, client_id, tagged_fields);
+       out.write(outputBuffer.array(), 0, outputBuffer.position());
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
@@ -53,5 +54,25 @@ public class Main {
          System.out.println("IOException: " + e.getMessage());
        }
      }
+  }
+
+  private static void processOutput(ByteBuffer outputBuffer, byte[] message_size, byte[] request_api_key, byte[] request_api_version, byte[] correlation_id, byte[] client_id, byte[] tagged_fields){
+
+
+	  outputBuffer.putInt(ByteBuffer.wrap(message_size).getInt());
+	  outputBuffer.putShort(ByteBuffer.wrap(request_api_key).getShort());
+	  short version = ByteBuffer.wrap(request_api_version).getShort();
+	  if(version < 0 || version > 4){
+		  outputBuffer.putShort(ByteBuffer.wrap(new byte[] {0, 35}).getShort());
+	  } else {
+		  outputBuffer.putShort(ByteBuffer.wrap(request_api_version).getShort());
+	  }
+	  outputBuffer.putInt(ByteBuffer.wrap(correlation_id).getInt());
+	  outputBuffer.putShort(ByteBuffer.wrap(client_id).getShort());
+	  outputBuffer.put(tagged_fields[0]);
+
+
+
+	
   }
 }
