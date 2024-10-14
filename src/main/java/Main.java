09312fbb4df2191d99394acc75d709e1c039febc
5hif7y <44300833+5hif7y@users.noreply.github.com>
2024-10-14 03:31:53 -0300

    added while loop to handle serial requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,30 +23,34 @@ public class Main {
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
 
-       // Input
-       InputStream in = clientSocket.getInputStream();
-       byte[] message_size = in.readNBytes(4); // INT32, 4 bytes
-       byte[] request_api_key = in.readNBytes(2); // INT16, 2 bytes
-       byte[] request_api_version = in.readNBytes(2); // INT16, 2 bytes
-       byte[] correlation_id = in.readNBytes(4); //INT32, 4 bytes
-       // client_id --- Nullable String --- Fixed to MAX INT16, 2 bytes, not sure if max INT64, 8 bytes
-       short client_id_length = ByteBuffer.wrap(in.readNBytes(2)).getShort();
-       byte[] client_id = client_id_length > 0 ? in.readNBytes(client_id_length) : new byte[0];
-       byte[] tagged_fields = in.readNBytes(1); // TAGGED_FIELDS
-
-       // Output
-       OutputStream out = clientSocket.getOutputStream();
-       
-       ByteBuffer outputBuffer = ByteBuffer.allocate(128);
-       processOutput(outputBuffer, message_size, request_api_key, request_api_version, correlation_id, client_id, tagged_fields);
-
-       // Calc total size of the message, excluding the first 4 bytes
-       int response_size = outputBuffer.position();
-       ByteBuffer finalOutputBuffer = ByteBuffer.allocate(4 + response_size);
-       finalOutputBuffer.putInt(response_size);
-       finalOutputBuffer.put(outputBuffer.array(), 0, response_size);
-
-       out.write(finalOutputBuffer.array());
+       while(true){ // Loop to handle multiple requests
+	
+	       // Input
+	       InputStream in = clientSocket.getInputStream();
+	       byte[] message_size = in.readNBytes(4); // INT32, 4 bytes
+	       if (message_size.length == 0) break; // ----- break loop conditional -----
+	       byte[] request_api_key = in.readNBytes(2); // INT16, 2 bytes
+	       byte[] request_api_version = in.readNBytes(2); // INT16, 2 bytes
+	       byte[] correlation_id = in.readNBytes(4); //INT32, 4 bytes
+	       // client_id --- Nullable String --- Fixed to MAX INT16, 2 bytes, not sure if max INT64, 8 bytes
+	       short client_id_length = ByteBuffer.wrap(in.readNBytes(2)).getShort();
+	       byte[] client_id = client_id_length > 0 ? in.readNBytes(client_id_length) : new byte[0];
+	       byte[] tagged_fields = in.readNBytes(1); // TAGGED_FIELDS
+	
+	       // Output
+	       OutputStream out = clientSocket.getOutputStream();
+	       
+	       ByteBuffer outputBuffer = ByteBuffer.allocate(128);
+	       processOutput(outputBuffer, message_size, request_api_key, request_api_version, correlation_id, client_id, tagged_fields);
+	
+	       // Calc total size of the message, excluding the first 4 bytes
+	       int response_size = outputBuffer.position();
+	       ByteBuffer finalOutputBuffer = ByteBuffer.allocate(4 + response_size);
+	       finalOutputBuffer.putInt(response_size);
+	       finalOutputBuffer.put(outputBuffer.array(), 0, response_size);
+	
+	       out.write(finalOutputBuffer.array());
+       }
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
@@ -79,13 +83,12 @@ public class Main {
 	  outputBuffer.putShort((short) 18);
 	  // MIN_VERSION --- INT16, 2bytes, short --- 0
 	  outputBuffer.putShort((short) 0);
-	  // MAX_VERSION --- INT16, 2bytes, short --- 4
+	  // MAX_VERSION --- INT16, 2 bytes, short --- 4
 	  outputBuffer.putShort((short) 4);
 	  // Throttle time --- INT32, 4 bytes --- 0
 	  outputBuffer.putInt((int) 0);
-	  // TAGGED_FIELDS --- 1 byte --- now 0 'no tagged fields' --- not sure if MAX INT64
-	  //outputBuffer.put((byte) 0);
-	  outputBuffer.putShort((short) 0); // test if this is ok
+	  // TAGGED_FIELDS --- INT16, 2 bytes --- now 0 'no tagged fields' --- not sure if MAX INT64
+	  outputBuffer.putShort((short) 0); 
 	
   }
 }
