7dcc103d547c3215e2280a19d43726815e0e4a0c
5hif7y <44300833+5hif7y@users.noreply.github.com>
2024-10-16 00:31:42 -0300

    New design based on new 'MessageUtils' utilities, decoupled from 'circular buffer' structure due to redesign. To be recoupled soon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,13 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.HexFormat;
+
 
 public class Main {
 
@@ -14,44 +18,103 @@ public class Main {
 	private static int THREAD_POOL_SIZE = 4;
 	private static int SOCKET_TIMEOUT_MS = 9000; // 9 seconds, 1 second less than test
 	
+	// Decoupling from CircularBuffer structure
 	// CircularBuffer implementation
-	private static int BUFFER_SIZE = 100;
-	private static CircularBuffer messageBuffer = new CircularBuffer(BUFFER_SIZE);
+	//private static int BUFFER_SIZE = 100;
+	//private static CircularBuffer messageBuffer = new CircularBuffer(BUFFER_SIZE);
 
   public static void main(String[] args){
 
-
      System.err.println("Logs from your program will appear here!");
 
-     ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-
-     try (ServerSocket serverSocket = new ServerSocket(PORT)){
-	     serverSocket.setReuseAddress(true);
-	     System.out.println("Server listening on port " + PORT);
-
-	     // Start consumer thread to process messages from the circular buffer
-	     executorService.submit(() -> KafkaProtocolUtils.consumeMessages(messageBuffer));
-	     
-	     // Wait for connection from client
-	     while(true){
-		     Socket clientSocket = serverSocket.accept();
-		     clientSocket.setSoTimeout(SOCKET_TIMEOUT_MS);
-		     executorService.submit(() -> handleClient(clientSocket));
-	     }
-
-       } catch (IOException e) {
-	     System.out.println("IOException: " + e.getMessage());
-     } finally {
-	     executorService.shutdown();
-	     try {
-		     if(!executorService.awaitTermination(60, TimeUnit.SECONDS)){
-			     executorService.shutdownNow();
-		     }
-	     } catch (InterruptedException ex){
-		     executorService.shutdownNow();
-	     }
+     try {
+	     ServerSocket serverSocket = new ServerSocket(PORT);
+	     ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+	
+	     if(serverSocket != null){
+		     handleConnections(serverSocket, executorService);
+     	     }
+     } catch (IOException e) {
+	     System.err.println("Error creating ServerSocket: " + e.getMessage());
      }
   }
+
+
+  private static ServerSocket createServerSocket(int port){
+	  try{
+		  ServerSocket serverSocket = new ServerSocket(port);
+		  serverSocket.setReuseAddress(true);
+		  return serverSocket;
+	  } catch (IOException e) {
+		  System.err.println("Failed to create server socket: " + e.getMessage());
+		  return null;
+	  }
+  }
+
+  private static void handleConnections(ServerSocket serverSocket, ExecutorService executorService){
+	  try{
+		  while(true){
+			  Socket clientSocket = serverSocket.accept();
+			  System.out.println("Accepted connection from " + clientSocket.getRemoteSocketAddress());
+			  executorService.submit(() -> KafkaClientHandler(clientSocket));
+		  }
+	  } catch (IOException e) {
+		  System.err.println("Error handling connections: " + e.getMessage());
+	  } finally {
+		  closeServerSocket(serverSocket);
+	  }
+  }
+
+  private static void closeServerSocket(ServerSocket serverSocket){
+	  if (serverSocket != null){
+		  try{
+			  serverSocket.close();
+		  } catch (IOException e){
+			  System.err.println("Failed to close server socket: " + e.getMessage());
+		  }
+	  }
+  }
+
+  
+
+  private static void KafkaClientHandler(Socket clientSocket) {
+
+	  HexFormat hexFormat = HexFormat.of();
+
+	  try (InputStream reader = clientSocket.getInputStream();
+	       OutputStream writer = clientSocket.getOutputStream()){
+
+		while(true) {
+			int messageLength = MessageUtils.readMessageLength(reader);
+			if(messageLength <= 0) break;
+
+			byte[] message = MessageUtils.readMessage(reader, messageLength);
+			if(message != null){
+				MessageUtils.processMessage(writer, message);
+			}
+		}
+	} catch (IOException e){
+		System.err.println("Error while handling client: " + e.getMessage());
+	} finally {
+		closeClientSocket(clientSocket);
+	}
+  }
+
+  private static void closeClientSocket(Socket clientSocket) {
+	  try{
+		  System.out.println("Connection from " + clientSocket.getRemoteSocketAddress() + " ended");
+		  clientSocket.close();
+	  } catch (IOException e) {
+		  System.err.println("Error closing client socket: " + e.getMessage());
+	  }
+  }
+
+}
+
+
+/*
+
+  
   
   // Modified for circular buffer implementation
   // Separating input and output behaviors as producer and consumer
@@ -106,4 +169,4 @@ public class Main {
 	  }
   }
 }
-
+*/
---
+++ src/main/java/MessageUtils.java
@@ -0,0 +1,99 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+
+public class MessageUtils {
+
+    public static int readMessageLength(InputStream reader) throws IOException {
+        byte[] lenWord = new byte[4];
+        if (reader.read(lenWord) == 4) {
+            ByteBuffer buffer = wrap(lenWord);
+            return buffer.getInt();
+        } else {
+            System.err.println("Message length not available");
+            return -1;
+        }
+    }
+
+    public static byte[] readMessage(InputStream reader, int messageLength) throws IOException {
+        byte[] message = new byte[messageLength];
+        if (reader.read(message) != messageLength) {
+            System.err.println("Message length does not match");
+            return null;
+        }
+        return message;
+    }
+
+    public static void processMessage(OutputStream writer, byte[] message) throws IOException {
+        ByteBuffer buffer = wrap(message);
+        //RequestKey key = RequestKey.fromValue(buffer.getShort());
+	//short key = (short)APIKeys.RespondAPIKeyRequest(buffer.getShort());
+	APIKeys key = APIKeys.fromApiKey(buffer.getShort());
+        int version = buffer.getShort();
+        int correlationId = buffer.getInt();
+        System.out.println("Received request for " + key + " " + version + " " + correlationId);
+
+        ByteBuffer responseBuffer = null;
+        switch (key) {
+            case APIKeys.API_VERSIONS:
+                responseBuffer = handleApiVersions(version, correlationId, key);
+                break;
+            // Handle other cases (PRODUCE, FETCH, HEARTBEAT)
+        }
+
+        if (responseBuffer != null) {
+            writer.write(data(responseBuffer));
+            writer.flush();
+        }
+    }
+
+ public  static ByteBuffer handleApiVersions(int version, int correlationId, APIKeys key) {
+        ByteBuffer message = createApiVersionsResponse(version, correlationId, key);
+        return createResponseBuffer(message);
+    }
+
+    public static ByteBuffer createApiVersionsResponse(int version, int correlationId, APIKeys key) {
+        ByteBuffer message = ByteBuffer.allocate(1024).order(ByteOrder.BIG_ENDIAN);
+        message.putInt(correlationId);
+        if (version >= 0 && version <= 4) {
+            message.putShort((short) 0); // No error
+            message.put((byte) 2) // compat arrays
+                    //.putShort(RequestKey.API_VERSIONS.type)
+                    .putShort((short)key.getCode())
+                    .putShort((short) 3) // Min version
+                    .putShort((short) 4) // Max version
+                    .put((byte) 0) // TAG BUFFER
+                    .putInt(0) // throttle_time_ms
+                    .put((byte) 0); // TAG BUFFER
+        } else {
+            message.putShort((short) 35); // Unsupported version error code
+        }
+        return message;
+    }
+
+    public static ByteBuffer createResponseBuffer(ByteBuffer message) {
+        ByteBuffer response = ByteBuffer.allocate(1024).order(ByteOrder.BIG_ENDIAN);
+        byte[] messageBytes = data(message);
+        response.putInt(messageBytes.length);
+        response.put(messageBytes);
+        return response;
+    }
+
+    public static byte[] data(ByteBuffer buffer) {
+        buffer.flip();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        return bytes;
+    }
+
+    public static ByteBuffer wrap(byte[] data) {
+        ByteBuffer buffer = ByteBuffer.wrap(data).order(ByteOrder.BIG_ENDIAN);
+        return buffer;
+    }
+}
+
+
+
